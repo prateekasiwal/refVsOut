5034f70ffbfd61b69b7a16ae697d5980ad928960
prateekasiwal <45721501+prateekasiwal@users.noreply.github.com>
2025-09-20 15:50:31 +0530

    Polymorphism  : Overloading and Overiding .

---
+++ refVsOut/BaseClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refVsOut;
+
+internal class BaseClass
+{
+    public virtual void Add(int num1, int num2)
+    {
+        Console.WriteLine($"Base is called {num1+num2}"); 
+    }
+}
+
+internal class ParentClass  : BaseClass
+{
+    public override void Add(int num1,int num2)
+    {
+        Console.WriteLine($"Parent is called {num1 * num2}");
+    }
+}
+
+
+
--- refVsOut/Program.cs
+++ refVsOut/Program.cs
@@ -1,5 +1,4 @@
 ﻿using refVsOut;
-using System;
 
 namespace MyApp
 {
@@ -8,20 +7,36 @@ namespace MyApp
         static void Main(string[] args)
         {
             //---------- Ref and Out Example -----------------
-            int add = 0;
-            int multi = 0;
+            //int add = 0;
+            //int multi = 0;
 
-            MyMethodRef(10, 20, ref add, ref multi);
-            Console.WriteLine($"Ref Add : {add}");
-            Console.WriteLine($"Ref Multi : {multi}");
+            //MyMethodRef(10, 20, ref add, ref multi);
+            //Console.WriteLine($"Ref Add : {add}");
+            //Console.WriteLine($"Ref Multi : {multi}");
 
             //MyMethodOut(10, 20, out int add, out int multi);
             //Console.WriteLine($"Out Add : {add}");
             //Console.WriteLine($"Out Multi : {multi}");
 
             //---------- Dependency Injection Example -----------------
-            MainService mainService = new(new MyService());
-             mainService.Run("Parteek");
+            //MainService mainService = new(new MyService());
+            // mainService.Run("Parteek");
+
+
+            //---------- Polymorphism OverRidding : Virtual and Override Example -----------------
+            ParentClass parentClass = new ParentClass();
+            parentClass.Add(10, 20);
+            BaseClass baseClass = new BaseClass();
+            baseClass.Add(10, 20);
+
+            BaseClass baseClass1 = new ParentClass();
+            baseClass1.Add(10, 20); //Parent class with virtual and override
+
+            //---------- Polymorphism Overloading ------------------
+            int var1 = dosomething(1, 2);
+            Console.WriteLine($"With two  parmater {var1}");
+            int var2 = dosomething(1, 2, 3);
+            Console.WriteLine($"With three  parmater {var2}");
         }
 
         public static void MyMethodRef(int num1, int num2, ref int add, ref int multi)
@@ -36,6 +51,15 @@ namespace MyApp
             multi = num1 * num2;
         }
 
+        public static int dosomething(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int dosomething(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
 
 
     }
